7d9bfaf88b33c675e2b87a71a548ecfa2479d8f1
CAO ANH KHOI <anhkhoi1221py@gmail.com>
2026-04-28 15:20:55 +0700

    Create game window

--- src/tetris/WindowGame.java
+++ src/tetris/WindowGame.java
@@ -6,7 +6,10 @@ public class WindowGame {
     public WindowGame() {
         window = new JFrame("Tetris ");
         window.setSize(WIDTH, HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
 
+        window.setVisible(true);
     }
     public static void main(String[] args) {
         new WindowGame();
